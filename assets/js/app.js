f8068d1baabf96c72a728d014ee4e3548f3b4262
Patilmahesh1 <patilmahesh759@gmail.com>
2023-03-26 13:01:35 +0530

    written code on edit and delete option

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,9 +8,21 @@ const title = document.getElementById("title");
 const image = document.getElementById("image");
 const rating = document.getElementById("rating");
 const moviecontainer = document.getElementById("moviecontainer");
+const submitbtn = document.getElementById("submitbtn")
+const updatebtn = document.getElementById("updatebtn")
 const myclose = [...document.querySelectorAll(".myclose")];
 
 let moviearray = JSON.parse(localStorage.getItem("moviearray")) || [];
+
+
+
+function uuid() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+      var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+      return v.toString(16);
+    });
+  }
+
 
 
 const temaplating = (arr) => {
@@ -19,7 +31,7 @@ const temaplating = (arr) => {
         result += `
          
         <div class="col-sm-4">
-        <div class="card">
+        <div class="card mb-3">
             <div class="card-header">
                 <h3>
                     ${obj.title}
@@ -28,10 +40,10 @@ const temaplating = (arr) => {
             <div class="card-body">
                 <img src="${obj.image}">
             </div>
-            <div class="card-footer text-right">
-                <p class="m-0 font-weight-bold">
-                    ${obj.rating}/5
-                </p>
+            <div class="card-footer">
+                <a href="#onEdit"><i class="fa-sharp fa-solid fa-pen-to-square mr-3" data-id="${obj.id}" onclick="onEdit(this)"></i></a>
+                <a href="#onDelete"><i class="fa-solid fa-trash text-danger" data-id="${obj.id}" onclick="onDelete(this)"></i></a>
+                <span class="float-right">${obj.rating}/5</span>
             </div>
         </div>
     </div>
@@ -39,6 +51,60 @@ const temaplating = (arr) => {
     })
     moviecontainer.innerHTML = result
 }
+
+
+
+const onEdit = (ele) => {
+    // cl(`Editted`,ele)
+    let editid = ele.getAttribute("data-id")
+    localStorage.setItem("updateId" , editid)
+    cl(editid)
+    let editobj = moviearray.find(obj => obj.id === editid)
+    cl(editobj)
+
+    title.value = editobj.title
+    image.value = editobj.image
+    rating.value = editobj.rating
+
+    submitbtn.classList.add("d-none")
+    updatebtn.classList.remove("d-none")
+    onmodalHandler()
+}
+
+
+const onDelete = (ele) => {
+    // cl("deleted")
+    let deleteId = ele.getAttribute("data-id")
+    cl(deleteId)
+    moviearray = moviearray.filter(movie => movie.id !== deleteId)
+    localStorage.setItem("moviearray", JSON.stringify(moviearray))
+    // temaplating(moviearray)
+    let close = ele.closest(".card")
+    close.remove()
+}
+
+
+
+
+const updateMovie = (eve) => {
+    let updateId = localStorage.getItem("updateId")
+    cl("updated...",updateId)
+    moviearray.forEach(movie => {
+        if(movie.id === updateId){
+            movie.title = title.value,
+            movie.image = image.value,
+            movie.rating = rating.value
+        }
+    })
+    localStorage.setItem("moviearray",JSON.stringify(moviearray))
+    temaplating(moviearray)
+    addmovie.reset()
+    updatebtn.classList.add("d-none")
+    submitbtn.classList.remove("d-none")
+    mymodal.classList.add("d-none")
+    overlay.classList.add("d-none")
+}
+
 
 
 const onmodalHandler = (eve) => {
@@ -50,9 +116,10 @@ const onaddmoviehandler = (eve) => {
     eve.preventDefault();
     // cl("add movies")
     let obj = {
-        title : title.value,
-        image : image.value,
-        rating : rating.value
+        title: title.value,
+        image: image.value,
+        rating: rating.value,
+        id:uuid()
     }
     moviearray.unshift(obj)
     localStorage.setItem("moviearray", JSON.stringify(moviearray));
@@ -63,7 +130,7 @@ const onaddmoviehandler = (eve) => {
     image.value = "";
     rating.value = "";
     onmodalHandler()
-  
+
 }
 temaplating(moviearray);
 
@@ -73,20 +140,21 @@ temaplating(moviearray);
 
 const ratingValueHandler = (eve) => {
     let val = eve.target.value;
-    if(val >= 1 && val <= 5){
+    if (val >= 1 && val <= 5) {
         eve.target.nextElementSibling.classList.add("d-none")
         return
-    }else{
+    } else {
         eve.target.nextElementSibling.classList.remove("d-none")
         eve.target.value = ""
     }
 }
 
 myclose.forEach(ele => {
-    ele.addEventListener("click" , onmodalHandler)
+    ele.addEventListener("click", onmodalHandler)
 })
 showmodelbtn.addEventListener("click", onmodalHandler)
 // addmovie.addEventListener("click" , onaddmoviehandler)
 addmovie.addEventListener("submit", onaddmoviehandler)
 
-rating.addEventListener("blur" , ratingValueHandler)
+rating.addEventListener("blur", ratingValueHandler)
+updatebtn.addEventListener("click", updateMovie)
